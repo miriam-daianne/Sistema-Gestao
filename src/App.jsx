85a5e643d62d51adc299c1637b6ec28c8db6976e
FehMell <139819379+FehMell@users.noreply.github.com>
2025-05-14 20:05:45 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Calculadora } from "./pages/Calculadora";
+import { Pacientes } from "./pages/Pacientes";
+import { Cadastro } from "./pages/Cadastro";
+import { Relatorio } from "./pages/Relatorio";
+import { Orcamento } from "./pages/Orcamento";
+
+
+
+function App() {
+
+
+  return (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/calculadora" element={<Calculadora />} />
+      <Route path="/pacientes" element={<Pacientes />}/>
+      <Route path="/cadastro" element ={<Cadastro />} />
+      <Route path="/relatorio" element={<Relatorio />}/>
+      <Route path="/orcamento" element={<Orcamento />}/>
+    </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default App
