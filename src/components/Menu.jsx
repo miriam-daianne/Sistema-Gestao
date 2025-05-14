8896d54fb96e97887675e8b78028d22bcb75a50a
FehMell <139819379+FehMell@users.noreply.github.com>
2025-05-14 20:06:30 -0300

    Add files via upload

---
+++ src/components/Menu.jsx
@@ -0,0 +1,35 @@
+import { NavLink } from 'react-router-dom';
+
+export function Menu() {
+    const menuItems = [
+        { label: 'Dashboard', to: '/dashboard'},
+        { label: 'Calculadora', to: '/calculadora' }, 
+        { label: 'Análise de Orçamento', to: '/orcamento' }, 
+        { label: 'Pacientes', to: '/pacientes' },
+        { label: 'Cadastro de Pacientes', to: '/cadastro' },
+        { label: 'Relatórios', to: '/relatorio'} 
+    ];
+
+    return (
+        <div className="bg-[#ECE6DF] rounded-md ml-4 p-2 w-fit">
+            <ul className="flex gap-3"> 
+                {menuItems.map((item, index) => (
+                    <li key={index}>
+                        <NavLink
+                            to={item.to}
+                            className={({ isActive }) =>
+                                `rounded-md px-3 py-1 text-sm font-medium transition-colors ${
+                                    isActive 
+                                        ? 'bg-[#A28567] text-white' 
+                                        : 'text-[#5e4d3b] hover:bg-[#A28567] hover:text-white'
+                                }`
+                            }
+                        >
+                            {item.label}
+                        </NavLink>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
