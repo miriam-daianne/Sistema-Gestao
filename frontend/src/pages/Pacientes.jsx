97a7737fac1ac8ee3099bc37f1caa548388dad1a
FehMell <139819379+FehMell@users.noreply.github.com>
2025-05-21 17:30:18 -0300

    Update Pacientes.jsx

--- frontend/src/pages/Pacientes.jsx
+++ frontend/src/pages/Pacientes.jsx
@@ -1,22 +1,21 @@
-import { Nav } from "../components/Nav"
-import { Menu } from "../components/Menu"
-import { Container } from "../components/Container"
-import { GestaoPacientes } from "../components/GestaoPacientes"
+import { Nav } from "../components/Nav";
+import { Menu } from "../components/Menu";
+import { Container } from "../components/Container";
+import { GestaoPacientes } from "../components/GestaoPacientes";
 
-export function Pacientes(){
-
-    return(
-       <div className="flex" >
-                 < Nav />
-                <div className="main ml-64 p-6 bg-[#FBFAF9] min-h-screen w-full flex flex-col">
+export function Pacientes() {
+  return (
+    <div className="flex">
+      <Nav />
+      <div className="main ml-64 p-6 bg-[#FBFAF9] min-h-screen w-full flex flex-col">
         <h2 className="text-[#A28567] font-semibold text-xl mb-4">
           Sistema de Cálculo de Comissão e Análise de Orçamentos
         </h2>
-                 < Menu />
-                <Container>   
-                    <GestaoPacientes />      
-               </Container>
-                </div>
-               </div>
-           )
-       }
+        <Menu />
+        <Container>
+          <GestaoPacientes />
+        </Container>
+      </div>
+    </div>
+  );
+}
