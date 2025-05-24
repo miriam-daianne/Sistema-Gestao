f2c71de61dbe95f26491f5571343bb520f470f8f
Mazzu <italo.itacom@gmail.com>
2025-05-24 00:12:01 -0300

    Delete API/db/migrate/AddStatusToConsultas.rb
    arquivo não era mais necessário

--- API/db/migrate/AddStatusToConsultas.rb
+++
@@ -1,6 +0,0 @@
-#uso: rails generate migration AddStatusToConsultas status:string
-class AddStatusToConsultas < ActiveRecord::Migration[7.0]
-  def change
-    add_column :consultas, :status, :string, default: 'agendado'
-  end
-end
