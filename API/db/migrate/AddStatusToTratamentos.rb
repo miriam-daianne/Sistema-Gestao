6958aa9ac6f4b58b4ecfb4e2d20868a14d34dbce
Mazzu <italo.itacom@gmail.com>
2025-05-24 00:13:05 -0300

    Delete API/db/migrate/AddStatusToTratamentos.rb

--- API/db/migrate/AddStatusToTratamentos.rb
+++
@@ -1,6 +0,0 @@
- #uso: rails generate migration AddStatusToTratamentos status:string
-class AddStatusToTratamentos < ActiveRecord::Migration[7.0]
-  def change
-    add_column :tratamentos, :status, :string, default: 'ativo'
-  end
-end
