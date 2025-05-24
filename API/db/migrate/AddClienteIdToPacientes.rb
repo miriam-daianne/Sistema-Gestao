39325c4b5aa32fb7ee1eaea9577c64eb12fe9439
Mazzu <italo.itacom@gmail.com>
2025-05-24 00:12:54 -0300

    Delete API/db/migrate/AddClienteIdToPacientes.rb
    Arquivo desnecessário

--- API/db/migrate/AddClienteIdToPacientes.rb
+++
@@ -1,6 +0,0 @@
-#uso: rails generate migration AddClienteIdToPacientes cliente:references
-class AddClienteIdToPacientes < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :pacientes, :cliente, foreign_key: true, null: true
-  end
-end
